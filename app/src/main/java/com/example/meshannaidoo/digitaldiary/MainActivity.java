51376538f06563bb7f5594d9b7fe7f45591b5770
Meshan Naidoo <meshann@gmail.com>
2017-03-02 21:30:50 +0200

    Added today button on landing page to create entry for today. Enabled the button when an entry for today didnt exist.

--- app/src/main/java/com/example/meshannaidoo/digitaldiary/MainActivity.java
+++ app/src/main/java/com/example/meshannaidoo/digitaldiary/MainActivity.java
@@ -3,7 +3,13 @@ package com.example.meshannaidoo.digitaldiary;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +18,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Calendar c = Calendar.getInstance();
+        final int year = c.get(Calendar.YEAR);
+        final int month = c.get(Calendar.MONTH);
+        final int dayOfMonth = c.get(Calendar.DAY_OF_MONTH);
+
+        File currentDirectory = getApplicationContext().getFilesDir();
+        currentDirectory.mkdirs();
+        String dayFileName = String.valueOf(year) + String.valueOf(month) + String.valueOf(dayOfMonth) + ".txt";
+        final File file = new File(currentDirectory, dayFileName);
+
+
+        Button todayButton = (Button)findViewById(R.id.MainButtonToday);
+
+        if(!file.exists())
+        {
+            todayButton.setEnabled(true);
+        }
+
+
+
         CalendarView calendarView = (CalendarView) this.findViewById(R.id.calendarView);
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                Intent link = new Intent(getApplicationContext(), DayActivity.class);
+                link.putExtra("year", year);
+                link.putExtra("month", month);
+                link.putExtra("dayOfMonth", dayOfMonth);
+                startActivity(link);
+            }
+        });
+        todayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
                 Intent link = new Intent(getApplicationContext(), DayActivity.class);
                 link.putExtra("year", year);
                 link.putExtra("month", month);
