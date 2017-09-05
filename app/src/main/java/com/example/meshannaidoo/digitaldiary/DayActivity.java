5bd280c8523c11863d6dcf24794daf1e727e3d4b
Meshan Naidoo <meshann@gmail.com>
2017-09-05 20:58:26 +0200

    experimentation with scroller

--- app/src/main/java/com/example/meshannaidoo/digitaldiary/DayActivity.java
+++ app/src/main/java/com/example/meshannaidoo/digitaldiary/DayActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Scroller;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -24,7 +25,6 @@ public class DayActivity extends AppCompatActivity {
         String year = String.valueOf(i.getIntExtra("year", 0));
         String month = String.valueOf(i.getIntExtra("month", 0));
         String dayOfMonth = String.valueOf(i.getIntExtra("dayOfMonth", 0));
-
 
         File currentDirectory = getApplicationContext().getFilesDir();
         currentDirectory.mkdirs();
@@ -55,17 +55,18 @@ public class DayActivity extends AppCompatActivity {
             br.close();
         }
         catch (IOException e) {
-            //You'll need to add proper error handling here
+
         }
 
         contents = text.toString();
 
         setContentView(R.layout.activity_day);
 
-
-
         EditText content = (EditText)this.findViewById(R.id.DayTextMain);
         content.setText(contents);
+
+        content.setScroller(new Scroller(getApplicationContext()));
+        content.setVerticalScrollBarEnabled(true);
 
         Button save = (Button)this.findViewById(R.id.DayButtonSave);
         save.setOnClickListener(new View.OnClickListener() {
@@ -76,9 +77,9 @@ public class DayActivity extends AppCompatActivity {
                 Save(file, content.getText().toString());
                 Intent link = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(link);
+                finish();
             }
         });
-
     }
 
     public void Save(File f, String newContents){
